2a5431c7e225373c6641a29e6823569b06c1c01c
Ptashynskyi <n.ptashynskyi@gmail.com>
2019-07-17 21:39:23 +0300

    add Marking Task as complete to list

--- src/components/todo-list-items/todo-list-items.js
+++ src/components/todo-list-items/todo-list-items.js
@@ -1,33 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import "./todo-list-items.css";
 
-const TodoListItems = ({ label, important = false }) => {
-  const style = {
-    color: important ? "steelblue" : "black",
-    fontWeight: important ? "bold" : "normal"
-  };
+export default class TodoListItems extends Component {
+  constructor() {
+    super();
+    this.state = {
+      done: false
+    };
+    this.makeDone = () => {
+      this.setState(({ done }) => {
+        return {
+          done: !done
+        };
+      });
+    };
+  }
+  render() {
+    const { label, important = false } = this.props;
+    const { done } = this.state;
+    let classNamesState = "todo-list-item";
+    if (done) {
+      classNamesState += " done";
+    }
+    const style = {
+      color: important ? "steelblue" : "black",
+      fontWeight: important ? "bold" : "normal"
+    };
 
-  return (
-    <span className="todo-list-item">
-      <span className="todo-list-item-label" style={style}>
-        {label}
+    return (
+      <span className={classNamesState}>
+        <span
+          className="todo-list-item-label"
+          style={style}
+          onClick={this.makeDone}
+        >
+          {label}
+        </span>
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa fa-trash-o" />
+        </button>
       </span>
-
-      <button
-        type="button"
-        className="btn btn-outline-success btn-sm float-right"
-      >
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button
-        type="button"
-        className="btn btn-outline-danger btn-sm float-right"
-      >
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItems;
+    );
+  }
+}
--- src/components/todo-state-btn/todo-state-btn.js
+++ src/components/todo-state-btn/todo-state-btn.js
@@ -1,21 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
 import "./todo-state-btn";
 
-const BtnGroup = () => {
-  return (
-    <div className="btn-group todo-state-btn" role="group">
-      <button type="button" className="btn btn-info">
-        All
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Active
-      </button>
-      <button type="button" className="btn btn-outline-secondary">
-        Done
-      </button>
-    </div>
-  );
-};
-
-export default BtnGroup;
+export default class BtnGroup extends Component {
+  render() {
+    return (
+      <div className="btn-group todo-state-btn" role="group">
+        <button type="button" className="btn btn-info">
+          All
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Active
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          Done
+        </button>
+      </div>
+    );
+  }
+}
