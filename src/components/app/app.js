5e008b64ae907f7dea2f24238f01d79ad025a1e5
Ptashynskyi <n.ptashynskyi@gmail.com>
2019-09-06 15:15:38 +0300

    add btn Add Item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 
 import AppHeader from "../app-header/app-header";
 import Search from "../search-panel/search-panel";
 import TodoList from "../todo-list/todo-list";
 import BtnGroup from "../todo-state-btn/todo-state-btn";
+import ItemAddForm from "../item-add-form/item-add-form";
 
 import "./app.css";
 
-const App = () => {
-  const todoData = [
-    { id: 1, label: "Drink tea", important: false },
-    { id: 2, label: "Create TodoApp", important: true },
-    { id: 3, label: "Take a walk", important: false },
-    { id: 4, label: "Learn Node.js", important: true }
-  ];
+export default class App extends Component {
+  genId = 10;
+  state = {
+    todoData: [
+      { id: 1, label: "Drink tea", important: false },
+      { id: 2, label: "Create TodoApp", important: true },
+      { id: 3, label: "Take a walk", important: false },
+      { id: 4, label: "Learn Node.js", important: true }
+    ]
+  };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <Search />
-        <BtnGroup />
+  deleteItem = id => {
+    this.setState(({ todoData }) => {
+      const indx = todoData.findIndex(el => el.id === id);
+
+      const newArr = [...todoData.slice(0, indx), ...todoData.slice(indx + 1)];
+
+      return { todoData: newArr };
+    });
+  };
+
+  addItem = text => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.genId++
+    };
+
+    this.setState(({ todoData }) => {
+      const newArr = [...todoData, newItem];
+
+      return { todoData: newArr };
+    });
+  };
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <Search />
+          <BtnGroup />
+        </div>
+        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+        <ItemAddForm addItem={this.addItem} />
       </div>
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
-
-export default App;
+    );
+  }
+}
--- src/components/todo-list-items/todo-list-items.js
+++ src/components/todo-list-items/todo-list-items.js
@@ -24,7 +24,7 @@ export default class TodoListItems extends Component {
     };
   }
   render() {
-    const { label } = this.props;
+    const { label, onDeleted } = this.props;
     const { done, important } = this.state;
     let classNamesMark = "todo-list-item";
     if (done) {
@@ -51,6 +51,7 @@ export default class TodoListItems extends Component {
         <button
           type="button"
           className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeleted}
         >
           <i className="fa fa-trash-o" />
         </button>
